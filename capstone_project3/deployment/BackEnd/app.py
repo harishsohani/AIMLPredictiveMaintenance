c798a3ddf71dbf7d444a73bb38c23229552d1a40
harishsohani <harishsohani@yahoo.com>
2026-02-15 09:37:13 +0000

    Update Project code to repository

--- capstone_project3/deployment/BackEnd/app.py
+++ capstone_project3/deployment/BackEnd/app.py
@@ -1,7 +1,14 @@
 
+# import for loading model
 import joblib
+
+# import pandas
 import pandas as pd
+
+# for creating APIs to serve requests 
 from flask import Flask, request, jsonify
+
+# import validation to check input
 from utils.validation import validate_and_prepare_input, InputValidationError
 
 # Initialize Flask app with a name
@@ -30,12 +37,96 @@ def predict_need_maintenance ():
 
         validated_df = validate_and_prepare_input(input_df, model)
 
-        prediction = model.predict(validated_df)[0]
+        prediction  = model.predict(validated_df)[0]
+        probability = model.predict_proba(validated_df)[0, 1]
 
         return jsonify({
             "status": "success",
-            "prediction": int(prediction)
+            "prediction": int(prediction),
+            "probability": float(probability)
         })
+
+    except InputValidationError as e:
+        return jsonify({
+            "status": "error",
+            "error_type": "validation_error",
+            "message": str(e)
+        }), 400
+
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "error_type": "internal_error",
+            "message": "Unexpected server error"
+        }), 500
+
+
+# Define an endpoint to predict sales for Super Kart
+@pred_mainteanance_api.post ('/v1/EngPredMaintenanceForBatch')
+def predict_need_maintenance_for_batch ():
+    
+    # validate request (json)
+    # if input is valid - return prediction
+    # in case of error - return appropriate error
+    try:
+        # Get the uploaded CSV file from the request
+        file = request.files.get('file')
+
+        if file is None:
+            return jsonify({
+                "status": "error",
+                "error_type": "input_error",
+                "message": "File not provided"
+            }), 400
+            
+        if file.filename == "":
+            return jsonify({
+                "status": "error",
+                "error_type": "input_error",
+                "message": "No file selected"
+            }), 400        
+    
+        # Read the file into a DataFrame
+        input_df = pd.read_csv (file)
+
+        if input_df.empty:
+            return jsonify({
+                "status": "error",
+                "error_type": "input_error",
+                "message": "Uploaded file is empty"
+            }), 400
+
+        
+        # Process the data to clean up and make it ready for prediction
+        # mostly we will use the file with same format as given in problem statement for batch prediction
+        
+        # remove/drop engine condition column if present
+        input_df.drop(columns=['Engine Condition'], inplace=True, errors='ignore')
+
+        # update column names to replace spaces with underscore
+        input_df.columns = input_df.columns.str.replace(' ', '_')
+
+        # Convert int → float
+        int_columns = input_df.select_dtypes(include=['int64']).columns
+        input_df[int_columns] = input_df[int_columns].astype('float64')
+        
+        # Validate entire batch
+        validated_df = validate_and_prepare_input(input_df, model)
+
+        # predict for given input
+        predictions   = model.predict(validated_df)
+        probabilities = model.predict_proba(validated_df)[:, 1]
+
+        # Convert numpy array → Python list
+        prediction_list    = predictions.tolist()
+        probabilities_list = probabilities.tolist()
+
+        return jsonify({
+                "status": "success",                     # overall batch status
+                "total_records": len(prediction_list),
+                "predictions": prediction_list,          # simple list version
+                "probabilities": probabilities_list,     # simple list version
+            })      
 
     except InputValidationError as e:
         return jsonify({
--- capstone_project3/deployment/FrontEnd/app.py
+++ capstone_project3/deployment/FrontEnd/app.py
@@ -7,53 +7,24 @@ import streamlit as st
 # import pandas
 import pandas as pd
 
-# define functiom which can provide formatted input with appropriate label and input text
-# this will help in p[roducing consistent representation
-def formatted_number_input(title, hint, **kwargs):
-    st.markdown(f"**{title}**")
-    st.caption(hint)
-    return st.number_input("", **kwargs)
-
-def formatted_number_input2(title, hint, minval, maxval, defvalue, steps, valformat="%.6f"):
-
-    st.markdown('<div style="margin-bottom:4px;">', unsafe_allow_html=True)
-
-    col1, col2 = st.columns([3, 1], vertical_alignment="center")
-
-    with col1:
-        st.markdown(
-            f"""
-            <div style="line-height:1.0">
-                <strong>{title}</strong><br>
-                <span style="font-size:1.10em; color:gray;">(Range {hint})</span>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    with col2:
-        usre_input = st.number_input("",
-                            min_value=minval,
-                            max_value=maxval,
-                            value=defvalue,
-                            step=steps,
-                            format=valformat,
-                            label_visibility="collapsed"
-                            )
-
-    st.markdown('</div>', unsafe_allow_html=True)
-
-    return usre_input
-
 # ---------------------------------------------------------
 # PAGE CONFIG
 # ---------------------------------------------------------
 st.set_page_config(
-    page_title="Predictive Maintenenace App"  #,
-    #layout="wide"
+    page_title="Predictive Maintenenace App",
+    layout="wide"
 )
 
 
+# ---------------------------------------------------------
+# TITLE
+# ---------------------------------------------------------
+#st.title("🏖️ Predict Engine Maintenance")
+#st.write("The Predict Maintenance app is a tool to predict if an Engine needs any maintenance based on provided operating sensor parameters.")
+#st.write("Fill in the details below and click **Predict** to see if the Engine needs maintenance to prevent from failure.")
+# -----------------------------
+# Title & Description
+# -----------------------------
 st.markdown("""
 <style>
 .block-container {
@@ -65,143 +36,339 @@ st.markdown("""
 
 
 # ---------------------------------------------------------
-# TITLE
+# Set Page TITLE and additional information for consumer
 # ---------------------------------------------------------
 st.title("🏖️ Predict Maintenance")
-st.write("The Predict Maintenance app is a tool to predict if an Engine needs any maintenance based on provided operating sensor parameters.")
-st.write("Fill in the details below and click **Predict** to see if the Engine needs maintenance to prevent from failure.")
+st.markdown("""
+The Predict Maintenance app help to predict if an engine needs maintenance based on operating sensor parameters.  
+*Suggested ranges are based on known information - input is not restricted to the specified range* 
+""")
 
 
+# generic function to provide input 
+# this is provided as an utiity to bring consistent user interface
+# currently few parameters are used, rest or for later expansion
+def formatted_number_input(title, hint, minval, maxval, defvalue, steps, valformat="%.4f"):
+
+    st.markdown('<div style="margin-bottom:4px;">', unsafe_allow_html=True)
+    
+    user_input = st.number_input(
+        label=f"{title}  ({hint})",
+        #min_value=minval,
+        #max_value=maxval,
+        value=defvalue,
+        #step=steps,
+        format=valformat,
+        #label_visibility="collapsed"
+    )
+         
+    return user_input
+
+
+st.markdown("""
+<style>
+/* Reduce top padding */
+.block-container {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+}
+/* Shared card styling */
+.card {
+    border-radius: 16px;
+    padding: 22px;
+    margin-bottom: 20px;
+    transition: 0.3s ease-in-out;
+}
+/* INPUT CARD */
+.input-card {
+    background: linear-gradient(145deg, #0f172a, #111827);
+    border: 1px solid #334155;
+    box-shadow: 0 0 0 1px rgba(59,130,246,0.15);
+}
+/* OUTPUT CARD */
+.output-card {
+    background: linear-gradient(145deg, #111827, #0b1220);
+    border: 1px solid #16a34a;
+    box-shadow: 0 0 12px rgba(34,197,94,0.25);
+}
+/* Card title */
+.card-title {
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 16px;
+    letter-spacing: 0.5px;
+}
+/* Button styling */
+div.stButton > button {
+    width: 100%;
+    border-radius: 10px;
+    height: 3em;
+    font-weight: 600;
+    background: linear-gradient(90deg, #2563eb, #1d4ed8);
+    color: white;
+    border: none;
+}
+div.stButton > button:hover {
+    background: linear-gradient(90deg, #1d4ed8, #1e40af);
+}
+</style>
+""", unsafe_allow_html=True)
 
 # ====================================
 # Section : Capture Engine Parameters
 # ====================================
-st.subheader ("Engine Parameters")
+#st.subheader ("Engine Parameters")
 
-rpm = formatted_number_input2(
-    "Lubricating oil pressure in kilopascals (kPa)",
-    "50 to 2500",
-    minval=50.0,
-    maxval=2500.0,
-    defvalue=735.0,
-    steps=10.0,
-    valformat="%.2f"
-)
+# divide UI into two column layout by defining two columns 
+# left column is used for input and right for output
+col_inputs, col_output = st.columns([3, 1.5])
+
+# update contnent (input) in left input column
+with col_inputs:
+
+    # using form for usre input, this proivides elegant interface
+    with st.form("engine_form"):
+
+        # set header string
+        st.subheader("🔧 Engine Parameters")
+
+        # create two columsn so we can spread the input into two columns
+        col_left, col_right = st.columns(2)
+    
+        # define inputs in left column
+        with col_left:
+        
+            rpm = formatted_number_input(
+                "Engine RPM",
+                "50 to 2500",
+                minval=50.0,
+                maxval=2500.0,
+                defvalue=735.0,
+                steps=10.0,
+                valformat="%.2f"
+            )
+            
+            
+            oil_pressure = formatted_number_input(
+                "Lubricating oil pressure in kPa",
+                "0.001 to 10.0",
+                minval=0.001,
+                maxval=10.0,
+                defvalue=3.300000,
+                steps=0.001,
+                valformat="%.6f"
+            )
+            
+            
+            fuel_pressure = formatted_number_input(
+                "Fuel Pressure in kPa",
+                "0.01 to 25.0",
+                minval=0.01,
+                maxval=25.0,
+                defvalue=6.500000,
+                steps=0.01,
+                valformat="%.6f"
+            )
+    
+        # define inputs in left column
+        with col_right:
+            coolant_pressure = formatted_number_input(
+                "Coolant Pressure in kPa",
+                "0.01 to 10.0",
+                minval=0.01,
+                maxval=10.0,
+                defvalue=2.250000,
+                steps=0.10,
+                valformat="%.6f"
+            )
+            
+            
+            lub_oil_temp = formatted_number_input(
+                "Lubricating oil Temperature in °C",
+                "50.0 to 100.0",
+                minval=50.0,
+                maxval=100.0,
+                defvalue=75.0,
+                steps=0.1,
+                valformat="%.6f"
+            )
+            
+            
+            coolant_temp = formatted_number_input(
+                "Coolant Temperature in °C",
+                "50.0 to 200.0",
+                minval=50.0,
+                maxval=200.0,
+                defvalue=75.000000,
+                steps=0.1,
+                valformat="%.6f"
+            )
+
+        submitted = st.form_submit_button("🚀 Check Maintenance")
 
 
-oil_pressure = formatted_number_input2(
-    "Lubricating oil pressure in kilopascals (kPa)",
-    "0.001 to 10.0",
-    minval=0.001,
-    maxval=10.0,
-    defvalue=3.300000,
-    steps=0.001,
-    valformat="%.6f"
-)
+with col_output:
 
+    # define place holders for output display
+    output_placeholder = st.empty()
+    probability_placeholder = st.empty()
+    details_placeholder = st.empty()
+    
 
-fuel_pressure = formatted_number_input2(
-    "Fuel Pressure in kilopascals (kPa)",
-    "0.01 to 25.0",
-    minval=0.01,
-    maxval=25.0,
-    defvalue=6.500000,
-    steps=0.01,
-    valformat="%.6f"
-)
+    # ==========================
+    # Single Value Prediction
+    # ==========================
+    with st.expander("🧠 Prediction Result", expanded=True):
+        # dispaly result only after submit is done
+        if submitted:
+        
+            # extract the data collected into a structure
+            input_data = {
+                'Engine_rpm'              : float(rpm),
+                'Lub_oil_pressure'        : float(oil_pressure),
+                'Fuel_pressure'           : float(fuel_pressure),
+                'Coolant_pressure'        : float(coolant_pressure),
+                'lub_oil_temp'            : float(lub_oil_temp),
+                'Coolant_temp'            : float(coolant_temp),
+            }
+        
+            input_df = pd.DataFrame([input_data])
+        
+            response = requests.post (
+                "https://harishsohani-AIMLProjectTestBackEnd.hf.space/v1/EngPredMaintenance",
+                json=input_data
+                )
+        
+            if response.status_code == 200:
+                ## get result as json
+                result = response.json ()
+        
+                resp_status = result.get ("status")
+        
+                if resp_status == "success":
+                    
+                    ## Get Sales Prediction, probability Values
+                    prediction_from_backend = result.get ("prediction")  # Extract only the value
+                    probability = result.get ("probability")  # Extract only the value
 
+                    # convert probability into % for representation
+                    formatted_prob = f"{probability * 100:.2f}%"
+            
+                    # generate output string
+                    if prediction_from_backend == 1:
+                        output_placeholder.error("⚠️ Engine needs maintenance")
+                    else:
+                        output_placeholder.success("✅ Engine operating normally")
 
-coolant_pressure = formatted_number_input2(
-    "Coolant Pressure in kilopascals (kPa)",
-    "0.01 to 10.0",
-    minval=0.01,
-    maxval=10.0,
-    defvalue=2.250000,
-    steps=0.10,
-    valformat="%.6f"
-)
+                    # dispaly probability of failur metric
+                    probability_placeholder.metric("Failure Probability", formatted_prob)
 
+                    # Display additional information
+                    details_placeholder.markdown(f"""
+                    *Model         :* XGBoost  
+                    *Inference     :* Real-time
+                    *Note*         : Probability of 50% and above is considered as Maintenance Needed.
+                    """)
+                    
+                else:
+        
+                    error_str = result.get ("message")
+    
+                    output_placeholder.error(f"⚠️ {error_str}")
+        
+            elif response.status_code == 400 or response.status_code == 500:  # known errors
+                
+                ## get result as json
+                result = response.json ()
+    
+                # get error message
+                error_str = result.get ("message")
+    
+                # show error message
+                output_placeholder.error(f"⚠️ Error processing request- Status Code : {response.status_code}, error : {error_str}")
+                
+            else:
+                output_placeholder.error(f"⚠️ Error processing request- Status Code : {response.status_code}")
+    
 
-lub_oil_temp = formatted_number_input2(
-    "Lubricating oil Temperature in degrees Celsius (°C)",
-    "50.0 to 100.0",
-    minval=50.0,
-    maxval=100.0,
-    defvalue=75.0,
-    steps=0.1,
-    valformat="%.6f"
-)
+# ==============================
+# Batch Prediction
+# ==============================
+st.markdown("---")
 
+st.subheader ("Batch Prediction for Engine Maintenance")
+st.markdown("""
+*Select csv file with engine sensor parameters to find prediction for all readings*
+""")
 
-coolant_temp = formatted_number_input2(
-    "Coolant Temperature in degrees Celsius (°C)",
-    "50.0 to 200.0",
-    minval=50.0,
-    maxval=200.0,
-    defvalue=75.000000,
-    steps=0.1,
-    valformat="%.6f"
-)
+file = st.file_uploader ("Upload CSV file", type=["csv"])
 
+if file is not None and st.button("🚀 Check Maintenance"):
 
-# ==========================
-# Single Value Prediction
-# ==========================
-if st.button("Check fo Maintenance"):
-
-    # extract the data collected into a structure
-    input_data = {
-        'Engine_rpm'              : float(rpm),
-        'Lub_oil_pressure'        : float(oil_pressure),
-        'Fuel_pressure'           : float(fuel_pressure),
-        'Coolant_pressure'        : float(coolant_pressure),
-        'lub_oil_temp'            : float(lub_oil_temp),
-        'Coolant_temp'            : float(coolant_temp),
-    }
-
-    input_df = pd.DataFrame([input_data])
-
-    response = requests.post (
-        "https://harishsohani-AIMLPredictiveMaintenanceBackEnd.hf.space/v1/EngPredMaintenance",
-        json=input_data
-        )
+    inputfile = {"file": (file.name, file.getvalue(), "text/csv")}
+    response = requests.post(
+        "https://harishsohani-AIMLProjectTestBackEnd.hf.space/v1/EngPredMaintenanceForBatch",
+        files=inputfile
+    )
 
     if response.status_code == 200:
-        ## get result as json
+        
         result = response.json ()
 
         resp_status = result.get ("status")
-
+        
         if resp_status == "success":
-
+                        
             ## Get Sales Prediction Value
-            prediction_from_backend = result.get ("prediction")  # Extract only the value
+            predictions_from_backend = result.get ("predictions")  # Extract only the value
 
-            # generate output string
-            if prediction_from_backend == 1:
-                resultstr = "Engine **likely** needs maintenance."
+            ## get probabbilities from back end
+            probabilities = result.get ("probabilities")  # Extract only the value
+
+            # read input data into data frame
+            input_df = pd.read_csv(file)
+
+            # Ensure lengths match
+            if len(predictions_from_backend) == len(input_df):
+    
+                # Add prediction and probability column
+                input_df["Prediction"]  = predictions_from_backend
+                input_df["Probability"] = probabilities
+    
+                st.success("Batch prediction completed successfully")
+
+                st.markdown("""
+                *Prediction  : 1 deontes Maintenance is needed*
+                """)
+
+                st.markdown("""
+                *Probability : This column indicates failure probability. Value ranges from 0 to 1. Value of 0.5 (50%) and above is considered as Maintenance Needed*
+                """)
+
+                st.markdown("""
+                """)
+    
+                # Show combined dataframe
+                st.dataframe(input_df, use_container_width=True)
+    
             else:
-                resultstr = "Engine does not need any maintenance"
+                st.error("Prediction count does not match input records")            
 
-            st.success(resultstr)
-
+            
         else:
 
             error_str = result.get ("message")
 
             st.error(error_str)
+        
 
     elif response.status_code == 400 or response.status_code == 500:  # known errors
-
+        
         ## get result as json
         result = response.json ()
 
         error_str = result.get ("message")
         st.error (f"Error processing request- Status Code : {response.status_code}, error : {error_str}")
-
+        
     else:
         st.error (f"Error processing request- Status Code : {response.status_code}")
-
-    # Show the etails of data frame prepared from user input
-    st.subheader("📦 Input Data Summary")
-    st.dataframe (input_df)
